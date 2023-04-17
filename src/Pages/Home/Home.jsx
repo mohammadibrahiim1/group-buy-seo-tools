1e7189e043c2b3d678f3120a414756b32c669509
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-04-17 16:46:42 +0600

    2 section design done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import AsSeenOn from "../../components/As-seen-on/AsSeenOn";
+import DigitalVision from "../../components/DigitaVision/DigitalVision";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>this is home page </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>this is home page </h1>
+      <AsSeenOn></AsSeenOn>
+      <DigitalVision></DigitalVision>
+    </div>
+  );
 };
 
 export default Home;
