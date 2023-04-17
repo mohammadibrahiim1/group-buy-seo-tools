8e2ab44933dbc57dcc50ee7ea21f080c24306c9c
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-04-17 21:11:59 +0600

    add two components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-// import AsSeenOn from "../../components/As-seen-on/AsSeenOn";
-// import DigitalVision from "../../components/DigitaVision/DigitalVision";
-import DigitalVision from '../../Components/DigitaVision/DigitalVision'
+import AsSeenOn from "../../Components/As-seen-on/AsSeenOn";
+import DigitalVision from "../../Components/DigitaVision/DigitalVision";
 
 const Home = () => {
   return (
     <div>
-
-      {/* <h1>this is home page </h1> */}
+      <AsSeenOn></AsSeenOn>
       <DigitalVision></DigitalVision>
-      {/* <AsSeenOn></AsSeenOn>
-      <DigitalVision></DigitalVision> */}
     </div>
   );
 };
---
+++ src/components/DigitaVision/DigitalVision.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./DigitalVision.css";
+
+const DigitalVision = () => {
+  return (
+    <div>
+      <section className="bg-[#F7F7F7]">
+        <div className="max-w-screen-xl mx-auto ">
+          <h1 className="text-4xl font-sans font-semibold section_title">
+            Why DigitalVision?
+          </h1>
+          <p className="text-center text-xl font-sans font-medium">
+            We make sure each penny you spend at DigitaVision worth it.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+            <article className="flex flex-col bg-white">
+              <img
+                src="https://digitavision.com/wp-content/uploads/2021/10/member-management.png"
+                alt=""
+                className="w-16 h-16 mx-auto rounded-full mt-4"
+              />
+              <div className="flex flex-col flex-1 p-6">
+                <h1>MEMBER MANAGEMENT SYSTEM</h1>
+                <h3 className="text-lg">
+                  We are the only provider that has a self-developed automated
+                  system that provides fast access to all our users with
+                  dashboard.
+                </h3>
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default DigitalVision;
