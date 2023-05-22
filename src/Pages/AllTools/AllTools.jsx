ed7b6794c07b5ad08e95f1ff8d355d46c15d997e
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-05-23 02:55:14 +0600

    all tools display in a table

--- src/Pages/AllTools/AllTools.jsx
+++ src/Pages/AllTools/AllTools.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import DisplayTools from "../../Components/DisplayTools/DisplayTools";
 
 const AllTools = () => {
   const [allTools, seeAllTools] = useState([]);
@@ -10,7 +11,79 @@ const AllTools = () => {
         seeAllTools(data);
       });
   }, []);
-  return <div>{allTools.length}</div>;
+  return (
+    <div>
+      <div>
+        <div className="overflow-x-auto w-full mx-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  {/* <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label> */}
+                </th>
+                <th>Tools Name</th>
+                <th>Active Status</th>
+                <th>Access System</th>
+                <th>Limits</th>
+                <th>Price</th>
+                <th>Cart</th>
+              </tr>
+            </thead>
+            {allTools.map((allTool, i) => (
+              <>
+                <tbody>
+                  <tr>
+                    <th>{i+1}</th>
+                    <td>
+                      <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={allTool.profile.image} alt="" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">
+                            {allTool.profile.name} 
+                          </div>
+                          {/* <div className="text-sm opacity-50">
+                            United States
+                          </div> */}
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      {allTool.profile.status}
+                      {/* <br />
+                      <span className="badge badge-ghost badge-sm">
+                        Desktop Support Technician
+                      </span> */}
+                    </td>
+                    <td>{allTool.profile.access}</td>
+                    <td>
+                    {allTool.profile.limit?.slice(0, 20)} ...
+                      <button className="btn btn-ghost btn-xs">
+                        see details
+                      </button>
+                    </td>
+                    <td>${allTool.profile.price}</td>
+                    <td>
+                      <button className="btn btn-info btn-xs">
+                        add to cart
+                      </button>
+                    </td>
+                  </tr>
+                  <hr />
+                </tbody>{" "}
+              </>
+            ))}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllTools;
---
+++ src/components/DisplayTools/DisplayTools.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DisplayTools = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default DisplayTools;
