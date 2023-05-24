62048bbfdc8e8f60050c36977a9937a08556e21e
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-05-24 16:55:11 +0600

    cart sidebar

--- src/components/CartDrawer/CartDrawer.jsx
+++ src/components/CartDrawer/CartDrawer.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import { FaCartArrowDown } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 // FaCartArrowDown
 
 const CartDrawer = () => {
+  const [openToggler, setOpenToggler] = useState(false);
   return (
     <div>
-      <div className="indicator ">
+      <div className="indicator" onClick={() => setOpenToggler(!openToggler)}>
         <span className="indicator-item badge badge-secondary">99+</span>
-        <button className="btn">
-          <FaCartArrowDown />
+        <button className=" btn btn-sm btn-primary flex flex-column">
+         <span> {openToggler ? "" : <FaCartArrowDown className=" h-5 w-5" />}</span> <br />
+          <span className=""> 100 BDT</span>
         </button>
+      </div>
+      <div
+        className={`cart-item ${
+          openToggler ? "cart-item-open" : "cart-item-closed"
+        }`}
+      >
+        <div className="logo " onClick={() => setOpenToggler(!openToggler)}>
+          {openToggler ? <MdClose className="h-8 w-8 " /> : ""}
+        </div>
+        <ul>
+          <li>item-1</li>
+          <li>item-2</li>
+        </ul>
       </div>
     </div>
   );
