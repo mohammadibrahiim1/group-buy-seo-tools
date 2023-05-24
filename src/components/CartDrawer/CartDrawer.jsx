be8c774282b8a0b770006ded564c295d53dd0bf6
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-05-24 17:13:57 +0600

    cart

--- src/components/CartDrawer/CartDrawer.jsx
+++ src/components/CartDrawer/CartDrawer.jsx
@@ -10,22 +10,38 @@ const CartDrawer = () => {
       <div className="indicator" onClick={() => setOpenToggler(!openToggler)}>
         <span className="indicator-item badge badge-secondary">99+</span>
         <button className=" btn btn-sm btn-primary flex flex-column">
-         <span> {openToggler ? "" : <FaCartArrowDown className=" h-5 w-5" />}</span> <br />
+          <span>
+            {" "}
+            {openToggler ? "" : <FaCartArrowDown className=" h-5 w-5" />}
+          </span>{" "}
+          <br />
           <span className=""> 100 BDT</span>
         </button>
       </div>
       <div
-        className={`cart-item ${
+        className={`cart-item bg-base-100 ${
           openToggler ? "cart-item-open" : "cart-item-closed"
         }`}
       >
-        <div className="logo " onClick={() => setOpenToggler(!openToggler)}>
-          {openToggler ? <MdClose className="h-8 w-8 " /> : ""}
+        <div
+          className="logo text-end mt-3"
+          onClick={() => setOpenToggler(!openToggler)}
+        >
+          <button className="btn btn-sm">
+            {" "}
+            {openToggler ? <MdClose className="h-5 w-5 " /> : ""}
+          </button>
         </div>
-        <ul>
-          <li>item-1</li>
-          <li>item-2</li>
-        </ul>
+        <div className="text-center">
+          <p className="text-sm text-error">
+            all in one package($250) 50$/30days
+          </p>
+          <p className="text-xs text-warning">save $200+ now</p>
+          <ul>
+            <li>item-1</li>
+            <li>item-2</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
