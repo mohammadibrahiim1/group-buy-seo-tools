db2fe3cfbf43fb68fe255c5bc9995224dd30526c
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-05-26 12:00:10 +0600

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className="header-section">
           <div className="text-center p-40">
             <div>
-              <h1 className="text-5xl ">There's a smarter way to do SEO.</h1>
+              <h1 className="text-5xl ">Valuable Group Buy SEO Tools Service at Affordable Price</h1>
               <p>
                 As a SEO / Digital Marketer / Website owner, you know how
                 important the SEO is. As a result, you're looking for ways to
@@ -19,13 +19,11 @@ const Header = () => {
                 you on the World Wide Web. If you're looking for affordable SEO
                 tools, look no further than Group Buy SEO Tools XYZ. This
                 company offers SEO tools for all sizes of businesses, at prices
-                everyone can afford.
-                <br />
-                India, Bangladesh, Pakistan, Nepal, United States, Saudi Arabia,
-                Malaysia, and many more countries peoples also can purchase our
-                service.
-                <br /> Our maximum tools have simple access system (One click to
-                access system) - hope you'll like this system!
+                everyone can afford. India, Bangladesh, Pakistan, Nepal, United
+                States, Saudi Arabia, Malaysia, and many more countries peoples
+                also can purchase our service. Our maximum tools have simple
+                access system (One click to access system) - hope you'll like
+                this system!
               </p>
             </div>
             {/* <div>
